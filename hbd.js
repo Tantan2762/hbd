7a7594192906151851787b4bdb9e87b525247e1c
Tantan2762 <tantan.nugraha@upi.edu>
2023-04-06 10:02:42 +0700

    ultah ica

---
+++ hbd.js
@@ -0,0 +1,5 @@
+alert("Cie ada yang ulang tahun nih!!!");
+let namaMu = prompt("Siapa sih nama yang lagi ulang tahun?");
+alert("Hallo Teteh " + namaMu + " Mau kejutan sederhana ga nih?(Kalo iya klik oke!)");
+let tgl = prompt("Sebelum liat kejutannya, harus isi password dulu, passwordnya tanggal lahir kamu (DD-MM-YYYY) :)");
+alert("Teteh " + namaMu + " yang baik hati lahir di tanggal "+tgl+ ", semoga suka ya :)");
